f39ddea5c1f867cfbaf439972874a993f9debd44
Williams <williams@demogr.mpg.de>
2019-05-23 13:14:16 +0200

    w/ tim fixed prev funct

--- PREVAR/R/test.R
+++ PREVAR/R/test.R
@@ -241,7 +241,6 @@ get_shift = function(t_star, alpha, lambda, S){
 		} 
 	}
 	
-	
 	((alpha * (S/2)) - (Area_lambda_S)) ^ 2
 }
 # get the within-lifespan prevalence function
@@ -270,9 +269,9 @@ get_shift = function(t_star, alpha, lambda, S){
 prev_lambda_alpha_y <- function(
 		x = seq(0,omega,by=delta), 
 		y = max(x), 
-		lambda = 1, 
-		alpha = 1, 
-		S = 5, 
+		lambda = 0, 
+		alpha = 1.5, 
+		S = 20, 
 		omega = 110,
 		delta = .05){
 
@@ -287,8 +286,11 @@ prev_lambda_alpha_y <- function(
 
   prev   <- x * 0
   if (sign(t_star) == 1){
-	  prev_x <- prev_sv(x = seq(t_star, S, by = delta), y = S, lambda = lambda)
-	  prev[x >= (y - S) & x <= (y - t_star)] <- prev_x
+	  prev_x <- prev_sv(x = seq(t_star, S, by = delta), y = S, lambda = lambda) 
+	  
+	  x_implied <- seq((y - S), (y - t_star), by = delta)
+	  
+	  prev[x >= (y - S) & x <= (y - t_star)] <- prev_x[x_implied>=0]
 	  prev[x > (y - t_star) & x <= y] <- 1
   }
   if (sign(t_star) == -1){
@@ -313,7 +315,6 @@ lines(prev_lambda_alpha_y(y=50,alpha=1,lambda=0,S=20))
 lines(prev_lambda_alpha_y(y=50,alpha=.5,lambda=0,S=20))
 lines(prev_lambda_alpha_y(y=50,alpha=1.5,lambda=0,S=20))
 lines(prev_lambda_alpha_y(y=50,alpha=.5,lambda=.5,S=20))
-prev_lambda_alpha_y(y=50,alpha=.5,lambda=.5,S=20)$prev
 # that is, get the marginal prevalence by age x assuming we specify 
 # lambda (single value or vector thereof)
 # alpha (single value or vector thereof)
@@ -334,24 +335,25 @@ prev_x_from_lambda_alpha <- function(
   # get ages when survival is 1, .99, .98, ..., .01, 0
   
   lives_lx <- life_bins(lx, 0:110, probs = q )[-1]
-  
+  lives_lx[lives_lx>omega] <- omega
   # vectorize to set flexible shapes by age
-  lambda = rep(lambda, length.out = length(lives_lx))
-  alpha = rep(alpha, length.out = length(lives_lx))
-  S = rep(S, length.out = length(lives_lx))
+  lambdas = rep(lambda, length.out = length(lives_lx))
+  alphas = rep(alpha, length.out = length(lives_lx))
+  Ss = rep(S, length.out = length(lives_lx))
   
   # get prevalence trajectory for each lifespan
   Prev_lifespan = matrix(0, ncol=length(x), nrow=length(lives_lx))
   for (i in 1:length(lives_lx)){
-    # i = 2
+    # i = 100
     Prev_lifespan[i,] = prev_lambda_alpha_y(
 			x = x, 
 			y = lives_lx[i], 
-			lambda = lambda[i], 
-			alpha = alpha[i], 
-			S = S[i],
+			lambda = lambdas[i], 
+			alpha = alphas[i], 
+			S = Ss[i],
 			delta = delta,
 			omega = omega)$y
+   
   }
   Prev_x <- colMeans(Prev_lifespan, na.rm = TRUE)
   # get mean
@@ -359,6 +361,7 @@ prev_x_from_lambda_alpha <- function(
 #  Prev_x = colSums(Prev_lifespan)/lives
   return(list(Prev_x = Prev_x, Prev_lifespan = Prev_lifespan, x = x))
 }
+Prev_lifespan <- prev_x_from_lambda_alpha(lx)$Prev_lifespan
 
 # For fixed rewards the variance is *zero* unless alpha, lambda, S vary over age.
 rowSums(Prev_lifespan,na.rm=TRUE) * delta
@@ -371,10 +374,14 @@ rowSums(Prev_lifespan,na.rm=TRUE) * delta
 # Now make a demonstrative plot for IW:
 q        <-  seq(1, 0, by = -.05)
 lives_lx  <- round(life_bins(lx, 0:110, probs = q)[-1])
+lives_lx[lives_lx>omega] <- omega
 xx        <- seq(0, 110, by = .1)
 S         <- lives_lx * .1 # 10% of life assume
 alphas    <- seq(.1, .5, length = length(lives_lx))
 lambdas   <- seq(5, .5, length = length(lives_lx))
+
+
+PrevMat <- matrix(ncol = 1101,nrow=20)
 
 plot(NULL, type = 'n', xlab = "Age", ylab = "Proportion", axes = FALSE, xlim = c(0,110), ylim=c(0,1))
 rect(0,q[-length(q)],lives_lx,q[-1], border = gray(.8))
@@ -382,7 +389,7 @@ lines(c(0,rep(lives_lx,each=2),0), rep(q,each=2))
 
 for (i in 1:length(lives_lx)){
 	pxi <- prev_lambda_alpha_y(
-			x = x, 
+			x = xx, 
 			y = lives_lx[i], 
 			lambda = lambdas[i], 
 			alpha = alphas[i], 
@@ -390,18 +397,22 @@ for (i in 1:length(lives_lx)){
 			delta = .1,
 			omega = omega)
 	xx <- pxi$x
-	pxi <- pxi$y
+	px <- pxi$y
 	# scale down
-	pxi <- pxi * .05
-	rem <- is.na(pxi) | pxi == 0
+	px <- px * .05
+	PrevMat[i, ] <- px
+	rem <- is.na(px) | px == 0
 	ends <- range(xx[!rem])
 	polygon(
 			x=c(ends[1],xx[!rem],ends[2]),
-			y=c(q[i+1], pxi[!rem] + q[i+1], q[i+1]) ,
+			y=c(q[i+1], px[!rem] + q[i+1], q[i+1]) ,
 			col = "black"
 	)
 }
 
+prev_L <- rowSums(PrevMat, na.rm=TRUE) 
+var_L = sum((prev_L - mean(prev_L)) ^ 2) / length(prev_L)
+var_L^.5/mean(prev_L)
 # -------------------------------------------------
 
 #prev_lifespan_inverse <- function(x, prev_lifespan,y){
@@ -434,7 +445,15 @@ Prev_lifespan <- prev_x_from_lambda_alpha(lx,
 		q = seq(1,0,by=-.002))$Prev_lifespan
 
 # a function to bin prevalence into single ages
-bin_prev_x <- function(prev_x){
+bin_prev_x <- function(prev_x, 
+                       xin = seq(0,110,by=.05), 
+                       xout = 0:100){
+  
+  trunc(xin) 
+  
+  for (i in 1:xout){
+    colSums(prev_x[,xout[i]])
+  }
 	
 }
 
