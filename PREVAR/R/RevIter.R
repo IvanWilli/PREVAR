f552f6dfb2338e469c369f92e06e55feb4e206c8
Williams <williams@demogr.mpg.de>
2019-06-24 17:51:44 +0200

    Reverse optimization. Some results

--- PREVAR/R/RevIter.R
+++ PREVAR/R/RevIter.R
@@ -1,14 +1,23 @@
-lives_lx = c(0.8, 1.1, 2.8, 3.9, 4.7, 5.1, 6.05, 7.6, 8.4, 10)
+lives_lx = c(0.8, 1.6, 2.1, 2.9, 4.7, 6.3, 6.56, 7.6, 8.7, 10)
 x = 0:10
-S = pmin(3,lives_lx)
-Pi_Obs = round(.8/(1+exp(-.1*(x[-length(x)]-7))),2)
-barplot(rev(lives_lx), horiz = T, col = 0, space = 0, xlim = c(0,max(x)), names.arg = rev(lives_lx))
+# lives_lx <- life_bins(lx, 0:110, probs = q )[-1]
+# lives_lx[100] = 100
+# x = 50:100
+# lives_lx = lives_lx[lives_lx>50]
+# S = pmin(10,lives_lx)
+
+# Pi_Obs = round(.6/(1+exp(-.1*(x[-length(x)]-5))),2) # bad fit
+Pi_Obs = .01*exp((x[-length(x)]+.05)*.4)
+plot(x[-length(x)], Pi_Obs, t='o')
+barplot(rev(lives_lx), horiz = T, col = 0, space = 0, xlim = range(x), names.arg = rev(lives_lx))
 abline(v=1:max(x), lty=2)
-mtext(text = Pi_Obs, side = 1, line = 2, at = seq(.6, max(x)-.4, 1), col = 2)
+mtext(text = round(Pi_Obs,2), side = 1, line = 2, at = seq(.6, max(x)-.4, 1), col = 2)
+mtext('Prev', side = 1, line = 2, at = -.5, col = 2, cex=.8)
 
 # how many PERSON-YEARS at each x
 alives = sapply(x[-length(x)], function(x) sum(pmin(pmax(lives_lx-x, 0), 1)))
 mtext(text = alives, side = 1, line = 3, at = seq(.6, max(x)-.4, 1), col = 4)
+mtext('Pers-years', side = 1, line = 3, at = -.5, col = 4, cex=.8)
 
 # groups and rounds
 
@@ -51,14 +60,10 @@ for(s in 1:length(rounds)){
 }
 rounds = rounds_mat
 
-
-# rounds = matrix(c(0,NA,NA,1,2,3,4,NA,NA), 3, 3, byrow = T)
-# groups = matrix(c(1, NA, 2, 3, 4, NA), 3, 2, byrow = T)
-
 # lambdas y matriz de prevalencias
 prevs = matrix(0, length(lives_lx), length(x)-1)
 colnames(prevs) = x[-length(x)]
-rownames(prevs) = lives_lx
+rownames(prevs) = round(lives_lx,2)
 lambda = c()
 
 # which lambda replicates prev in tha
@@ -67,13 +72,16 @@ for(i in nrow(groups):1){
     lives_i = groups[i,][!is.na(groups[i,])]
     ages = rounds[i,][!is.na(rounds[i,])]
     
-    lambda[lives_i] = optimize(get_lambda, lower = -50, upper = 50, 
-                               x_ini = min(ages), x_fin = lives_lx[lives_i],
+    
+    
+    
+    lambda[lives_i] = optimize(get_lambda_sim, lower = -10, upper = 10, 
+                               x_ini = x[min(ages)+1], x_fin = lives_lx[lives_i],
                                PrevObs_x = sum(Pi_Obs[ages+1] * alives[ages+1]) - 
-                                           ifelse(i==nrow(groups), 0, sum(prevs[max(lives_i):nrow(prevs), ages+1])), 
+                                           ifelse(i==nrow(groups), 0, 
+                                                  sum(prevs[max(lives_i):nrow(prevs), ages+1])), 
                                S = S[lives_i], 
-                               lives = length(lives_i))$minimum
-
+                               lives = length(lives_i), tol = 1e-10)$minimum
     
     for(live in lives_i){
       prevs[live,] = sapply(x[-length(x)], 
@@ -81,14 +89,9 @@ for(i in nrow(groups):1){
                                                                        y = lives_lx[live], S = S[live], 
                                                                        lambda = lambda[live],
                                                                        subdivisions = 20000)$value)
+      
     }
 }
-
-#chek!
-Pi_Obs * alives; colSums(prevs)
-round(Pi_Obs * alives[-6] - colSums(prevs),5)
-sum((Pi_Obs * alives)[3:5])
-sum(colSums(prevs)[3:5])
 
 # graph
 for(s in 1:length(lives_lx)){
@@ -96,10 +99,15 @@ for(s in 1:length(lives_lx)){
                                                  y = lives_lx[s], S = S[s], 
                                                  lambda = lambda[s])+(length(lives_lx)-s))
 }
+
+#chek!
+fit = round(matrix(c(Pi_Obs * alives, colSums(prevs)), nrow = 2, ncol = length(alives), byrow = T), 2)
+barplot(fit, beside = T, names.arg = x[-length(x)], legend.text = c('Obs', 'Fit'), ylab = 'PY prevalence')
 plot(lambda)
 
-# get lambda function
-get_lambda = function(lambda, S, x_ini, x_fin, PrevObs_x, lives){
+# get lambda sim function
+get_lambda_sim = function(lambda, S, x_ini, x_fin, PrevObs_x, lives){
+  # x_ini=8; x_fin=c(8.7); S=5; lambda=.5; lives=1
   Prev_x = c()
   for(j in 1:lives){
     Prev_x[j] = integrate(f = prev_sv_simetric, lower =  x_ini, upper = x_fin[j], subdivisions = 20000, 
@@ -108,19 +116,22 @@ get_lambda = function(lambda, S, x_ini, x_fin, PrevObs_x, lives){
   (PrevObs_x - sum(Prev_x))^2
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# prev function
+prev_sv_simetric = function(x, y, S, lambda=3){ # x vector of ages at death
+  # x = c(1.5, 2, 2.5, 3, 3.5, 3.7, 4, 4.5); y = 4; S =2; lambda = -1
+  xS = pmax(x-(y-S), 0)
+  p <- (xS * exp(abs(lambda) * (xS - S)))/S
+  p[p > 1 | is.nan(p)] <- 0
+  if(lambda<0){
+    xS = pmax(S-(x-(y-S)), 0)
+    p <- (xS * exp(abs(lambda) * (xS - S)))/S
+    p[x>=(y-S) & x<=y] = 1-p[x>=(y-S) & x<=y]
+    p[p > 1 | is.nan(p)] <- 0
+  }
+  p
+}
+plot(0,0, col=0, ylim = c(0,1), xlim=c(0,10), xlab='x', ylab='prev', asp=T)
+for (lambdas in seq(-10, 10, .5)){
+  lines(seq(0, 10, .05), prev_sv_simetric(seq(0, 10, .05), y = 10, S = 5, lambdas), 
+        col=ifelse(lambdas>0, 1, 2))
+}
